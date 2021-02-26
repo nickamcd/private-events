c0baa013d96c4d4ed725fddeaccd568addc5e9a6
Nick McDaniel <nickamcd@gmail.com>
2021-02-26 14:29:30 -0800

    Added model for Events

---
+++ db/migrate/20210226215505_create_events.rb
@@ -0,0 +1,14 @@
+class CreateEvents < ActiveRecord::Migration[6.1]
+  def change
+    create_table :events do |t|
+      t.integer :host_id
+      t.string :title
+      t.date :start_date
+      t.date :end_date
+      t.time :start_time
+      t.time :end_time
+
+      t.timestamps
+    end
+  end
+end
