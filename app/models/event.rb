7c214ddc8644830298fc6c3a852c1367c585c606
Nick McDaniel <nickamcd@gmail.com>
2021-02-28 10:35:51 -0800

    Can display user's upcoming events

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,3 +1,6 @@
 class Event < ApplicationRecord
-  belongs_to :creator, class_name: 'User', foreign_key: 'user_id'
+  belongs_to :creator, class_name: 'User'
+
+  scope :previous, -> { where('start_date > ?', Time.zone.today) }
+  scope :future, -> { where('start_date < ?', Time.zone.today) }
 end
