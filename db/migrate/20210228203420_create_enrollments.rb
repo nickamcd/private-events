c4e51a3a7d1df9deab52ad6c3debf67a394f7293
Nick McDaniel <nickamcd@gmail.com>
2021-02-28 15:53:05 -0800

    Event displays attendees and invitees

--- db/migrate/20210228203420_create_enrollments.rb
+++ db/migrate/20210228203420_create_enrollments.rb
@@ -1,8 +1,8 @@
 class CreateEnrollments < ActiveRecord::Migration[6.1]
   def change
     create_table :enrollments do |t|
-      t.references user, foreign_key: true
-      t.references event, foreign_key: true
+      t.references :user, foreign_key: true
+      t.references :event, foreign_key: true
 
       t.timestamps
     end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_02_28_185457) do
+ActiveRecord::Schema.define(version: 2021_02_28_203420) do
+
+  create_table "enrollments", force: :cascade do |t|
+    t.integer "user_id"
+    t.integer "event_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["event_id"], name: "index_enrollments_on_event_id"
+    t.index ["user_id"], name: "index_enrollments_on_user_id"
+  end
 
   create_table "events", force: :cascade do |t|
     t.integer "creator_id"
@@ -38,4 +47,6 @@ ActiveRecord::Schema.define(version: 2021_02_28_185457) do
     t.index ["username"], name: "index_users_on_username", unique: true
   end
 
+  add_foreign_key "enrollments", "events"
+  add_foreign_key "enrollments", "users"
 end
